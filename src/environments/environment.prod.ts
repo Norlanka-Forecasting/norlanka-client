be6a03d82ce87ea8f1386c9f98e3c892b9f09589
Isira Uditha <isira@edhirya.com>
2022-12-02 01:42:40 +0530

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:4000',
+  // baseUrl: 'http://127.0.0.1:5000',
+  baseUrl: 'https://norlanka-pulse.herokuapp.com',
+  role: 'manager',
 };
